b80852ab78392e523340316a81e39127621160d3
Aleksey700rmm <92338644+Aleksey700rmm@users.noreply.github.com>
2022-05-14 01:12:05 +0300

    Before 138 practice

---
+++ src/components/app/app.js
@@ -0,0 +1,118 @@
+import { Component } from 'react';
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+
+import './app.css'
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
+            ],
+            term: ''
+        }
+        this.maxId = 4;
+    }
+
+    deleteItem =(id) => {
+        this.setState(({data}) => {
+            // const index = data.findIndex(elem => elem.id === id);
+            
+            // const before = data.slice(0, index);
+            // const after = data.slice(index + 1);
+
+            // const newArr = [...before, ...after];
+
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            rise: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    onToggleIncrease = (id) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return{...item, increase: !item.increase}
+                }
+                return item;
+            })
+        }))
+    }
+
+    onToggleRise = (id) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return{...item, rise: !item.rise}
+                }
+                return item;
+            })
+        }))
+    }
+
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term}); //То же что и term: term
+    }
+
+    render() {
+        const {data, term} = this.state;
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.searchEmp(data, term);
+
+        return (
+            <div className='app'>
+                <AppInfo employees={employees} increased={increased}/>
+    
+                <div className="search-panel">
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployersList 
+                    data={visibleData}
+                    onDelete={this.deleteItem}
+                    onToggleIncrease={this.onToggleIncrease}
+                    onToggleRise={this.onToggleRise}/>
+                <EmployersAddForm onAdd={this.addItem}/>
+            </div>
+        )
+    }
+}
+
+export default App;
