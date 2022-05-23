c56dc67322b7fc4a5be7d0d7702a173d82c97452
Aleksey700rmm <92338644+Aleksey700rmm@users.noreply.github.com>
2022-05-24 01:44:59 +0300

    Almost final version

--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -14,7 +14,9 @@ const EmployersListItem = (props) => {
 
     return (
         <li className={classNames}>
-            <span className="list-group-item-label" onClick={onToggleRise}>{name}</span>
+            <span className="list-group-item-label" onClick={onToggleRise}
+            // style={{fontSize: '40px', color: 'red', transition: 'all', WebkitTransition: 'all', msTransition: 'all'}}
+            >{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className="d-flex justify-content-center align-items-center">
                 <button type="button"
